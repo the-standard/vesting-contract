8668253871b96f248abc2ff36b2c7a7e03b2d234
Simon Morley <simon.morley@mac.com>
2021-06-22 18:10:03 +0100

    Tests

--- test/vesting.test.js
+++ test/vesting.test.js
@@ -75,6 +75,15 @@ contract('VestingVault12CreateGrants', function ([ creator, other ]) {
     await expectEvent.inLogs(logs, 'GrantAdded', { recipient: address });
     await expectEvent.inLogs(logs, 'GrantAdded', { vestingId: '0' });
 
+    // check the tokenGrant returns correctly
+    const ag = await this.contract.tokenGrants(0);
+    expect(ag.startTime).to.be.bignumber.eq(startTime.toString());
+    expect(ag.amount).to.be.bignumber.eq(amount.toString());
+    expect(ag.vestingDuration).to.be.bignumber.eq(duration.toString());
+    expect(ag.vestingCliff).to.be.bignumber.eq('0');
+    expect(ag.daysClaimed).to.be.bignumber.eq('0');
+    expect(ag.recipient).to.eq(address);
+
     // might as well check the active grants in here
     grants = await this.contract.getActiveGrants(address);
     expect(grants.length).to.eq(1);
@@ -83,6 +92,55 @@ contract('VestingVault12CreateGrants', function ([ creator, other ]) {
     expect(grants[0]).to.be.bignumber.equal(grant)
 
     // since we have a grant now, lettuce check the data for it
+    // Check the tokensVestedPerDay
+    const daily = new BN('27397'); // amount / duration;
+    const vested = await this.contract.tokensVestedPerDay(0);
+    expect(vested).to.be.bignumber.equal(daily);
+
+    // check calculateGrantClaim
+    const claim = await this.contract.calculateGrantClaim(0);
+    expect(claim[0]).to.be.bignumber.equal(new BN('0'));
+    expect(claim[1]).to.be.bignumber.equal(new BN('0'));
+  });
+  
+  it('checks the events from the addTokenGrant with earlier start date', async function () {
+    const address = '0x2F2E2Ed85CB968FD3315AE402997B45eC7fe0643';
+    const startTime = 0; // this is supposed to be at some point in the past
+    const amount = 10000000;
+    const duration = 10;
+    const cliff = 0;
+
+    // Mint the tokens to the creator
+    await this.token.mint(creator, TOTAL_SUPPLY);
+
+    // increase the allowance of the contract
+    await this.token.increaseAllowance(this.contract.address, TOTAL_SUPPLY);
+
+    // // check the contract has all the tokens
+    expect(await this.token.allowance(creator, this.contract.address)).to.be.bignumber.equal(TOTAL_SUPPLY)
+
+    const { logs } = await this.contract.addTokenGrant(address, startTime, amount, duration, cliff);
+
+    await expectEvent.inLogs(logs, 'GrantAdded', { recipient: address });
+    await expectEvent.inLogs(logs, 'GrantAdded', { vestingId: '0' });
+
+    // // might as well check the active grants in here
+    // grants = await this.contract.getActiveGrants(address);
+    // expect(grants.length).to.eq(1);
+
+    // const grant = new BN('0');
+    // expect(grants[0]).to.be.bignumber.equal(grant)
+
+    // // since we have a grant now, lettuce check the data for it
+    // // Check the tokensVestedPerDay
+    // const daily = new BN('27397'); // amount / duration;
+    // const vested = await this.contract.tokensVestedPerDay(0);
+    // expect(vested).to.be.bignumber.equal(daily);
+
+    // check calculateGrantClaim
+    const claim = await this.contract.calculateGrantClaim(0);
+    expect(claim[0]).to.be.bignumber.equal(new BN('0'));
+    expect(claim[1]).to.be.bignumber.equal(new BN('0'));
   });
 });
 
